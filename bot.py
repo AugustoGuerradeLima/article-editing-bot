a9725b0a9ccfd94e93a47e4ca32232426058e4e0
Augusto Guerra de Lima <103451503+AugustoGuerradeLima@users.noreply.github.com>
2023-02-16 04:20:10 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import pyautogui
 from time import sleep
 
-dirname = "C:\\sourcecode.txt"#just an exemple
+dirname = "C:\\Users\\Augus\\Desktop\\psi\\sourcecode.txt"
 
 ########################################################################1
-#take source code
-pyautogui.press('win')
-pyautogui.write(dirname)
-pyautogui.press('enter')
-pyautogui.click(1314,511,duration=1)#generic
-pyautogui.hotkey('ctrl','a')
-pyautogui.hotkey('ctrl','c')
-pyautogui.hotkey('alt','f4')
+def takeSourceCode():
+    pyautogui.press('win')
+    pyautogui.write(dirname)
+    pyautogui.press('enter')
+    pyautogui.click(1314,511,duration=1)#generic
+    pyautogui.hotkey('ctrl','a')
+    pyautogui.hotkey('ctrl','c')
+    pyautogui.hotkey('alt','f4')
+
+########################################################################main
+takeSourceCode()
